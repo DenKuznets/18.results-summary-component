0da3f724aa721a385c5f6d88ebbfb547de792c91
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-23 16:36:19 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import GlobalStyles from './Global'
+
+
+function App() {
+
+  return (
+    <div className="App">
+     <GlobalStyles />
+     HELLO
+    </div>
+  )
+}
+
+export default App
