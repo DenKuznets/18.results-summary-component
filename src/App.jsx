c4ce92e9ef8d2d400bb592ebba644e7e1afafded
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-25 11:54:35 +0300

    theme updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,22 @@ import Flex from "./components/styles/Flex.styled";
 const theme = {
   mobile: "768px",
   colors: {
-    LighRed: "hsl(0, 100%, 67%)",
-    OrangeyYellow: "hsl(39, 100%, 56%)",
-    GreenTeal: "hsl(166, 100%, 37%)",
-    CobaltBlue: "hsl(234, 85%, 45%)",
+    primary: {
+      LighRed: "hsl(0, 100%, 67%)",
+      OrangeyYellow: "hsl(39, 100%, 56%)",
+      GreenTeal: "hsl(166, 100%, 37%)",
+      CobaltBlue: "hsl(234, 85%, 45%)",
+    },
+    neutral: {
+      White: "hsl(0, 0%, 100%)",
+      PaleBlue: "hsl(221, 100%, 96%)",
+      LightLavender: "hsl(241, 100%, 89%)",
+      DarkGrayBlue: "hsl(224, 30%, 27%)",
+    },
+  },
+  gradients: {
+    result: "hsl(252, 100%, 67%), hsl(241, 81%, 54%)",
+    score: "hsla(256, 72%, 46%, 1), hsla(241, 72%, 46%, 0)",
   },
 };
 
