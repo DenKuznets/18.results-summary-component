651604a639749a58a16eadd647a0d35c76bac41d
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-02-28 13:05:09 +0300

    result mobile and desktop done

--- src/components/styles/Result.styled.js
+++ src/components/styles/Result.styled.js
@@ -7,24 +7,28 @@ const StyledResult = styled.div`
   border-radius: 0 0 36px 36px;
   width: 100%;
   height: 357px;
+  font-size: 18px;
 
-  @media (min-width: ${({theme}) => theme.mobile}) {
+  @media (min-width: ${({ theme }) => theme.mobile}) {
     height: 515px;
     border-radius: 36px;
+    font-size: 26px;
   }
 
   header {
     opacity: 0.7;
-    margin-top: 1.2rem;
-    
+    margin-top: 1.4em;
+    @media (min-width: ${({ theme }) => theme.mobile}) {
+      font-size: 1.4rem;
+    }
   }
 
   .result__score {
-    margin-top: 1.2rem;
+    margin-top: 1.2em;
     border-radius: 50%;
     background: linear-gradient(${({ theme }) => theme.gradients.score});
-    height: 8rem;
-    width: 8rem;
+    height: 8em;
+    width: 8em;
 
     .result__flex {
       height: 100%;
@@ -33,20 +37,20 @@ const StyledResult = styled.div`
       .result__user-score {
         font-size: 3rem;
         font-weight: 700;
-        line-height: 3.8rem;
-        @media (min-width: ${({theme}) => theme.mobile}) {
-          line-height: 3.2rem;
-          font-size: 2.8rem;
+        line-height: 1.3;
+        @media (min-width: ${({ theme }) => theme.mobile}) {
+          line-height: 1.15;
+          font-size: 3.8rem;
         }
       }
 
       .result__total-score {
-        line-height: 1.2rem;
+        line-height: 1.2;
         opacity: 0.5;
         font-size: 0.9rem;
-        @media (min-width: ${({theme}) => theme.mobile}) {
-          font-size: 0.7rem;
-          line-height: 0.6rem;
+        @media (min-width: ${({ theme }) => theme.mobile}) {
+          /* font-size: 0.7rem; */
+          line-height: 1;
         }
       }
     }
@@ -55,11 +59,11 @@ const StyledResult = styled.div`
   .result__info {
     h1 {
       font-size: 1.3rem;
-      margin-top: 1.1rem;
+      margin-top: 0.7em;
       line-height: 1.7;
-      @media (min-width: ${({theme}) => theme.mobile}) {
-        font-size: 1.2rem;
-        line-height: 1.1rem;
+      @media (min-width: ${({ theme }) => theme.mobile}) {
+        font-size: 1.8rem;
+        line-height: 1.2;
       }
     }
     p {
@@ -68,11 +72,10 @@ const StyledResult = styled.div`
       max-width: 260px;
       line-height: 1.5;
       font-weight: 300;
-      /* letter-spacing: 1px; */
-      @media (min-width: ${({theme}) => theme.mobile}) {
-        font-size: 0.7rem;
-        margin-top: 24px;
-        line-height: 0.9rem;
+      @media (min-width: ${({ theme }) => theme.mobile}) {
+        font-size: 1rem;
+        margin-top: 1em;
+        line-height: 1.4;
         max-width: 260px;
       }
     }
